7aa90e67b430b4ec56234f6684ae3865889d1e3b
Nemo <Nemo@DESKTOP-P0P6JU6>
2016-10-23 20:12:27 +0700

    File manipulations

---
+++ src/WriteBinFile.java
@@ -0,0 +1,43 @@
+import java.io.*;
+
+public class WriteBinFile {
+    public static void main(String [] args) {
+
+        // The name of the file to create.
+        String fileName = "data/Writetemp.txt";
+
+        try {
+            // Put some bytes in a buffer so we can
+            // write them. Usually this would be
+            // image data or something. Or it might
+            // be unicode text.
+            String bytes = "Hello theren";
+            byte[] buffer = bytes.getBytes();
+
+            FileOutputStream outputStream =
+                new FileOutputStream(fileName);
+
+            // write() writes as many bytes from the buffer
+            // as the length of the buffer. You can also
+            // use
+            // write(buffer, offset, length)
+            // if you want to write a specific number of
+            // bytes, or only part of the buffer.
+            outputStream.write(buffer);
+
+            // Always close files.
+            outputStream.close();       
+
+            System.out.println("Wrote " + buffer.length + 
+                " bytes");
+        }
+        catch(IOException ex) {
+            System.out.println(
+                "Error writing file '"
+                + fileName + "'");
+            // Or we could just do this:
+            // ex.printStackTrace();
+        }
+    }
+}
+
